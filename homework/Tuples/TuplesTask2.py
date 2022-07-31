4c39aeb584bd9343c3816a92a73b3cbf0c20f212
vitali_saharov <vitali.saharov@gmail.com>
2022-07-31 13:34:23 +0300

    SetsTask2 completed

--- homework/Tuples/TuplesTask2.py
+++ homework/Tuples/TuplesTask2.py
@@ -1,2 +1,13 @@
-string_a = input('Введите текст: ')
-print(tuple(filter(str.isalpha, string_a)))
+# пользователь вводит строку
+# преобразовать строку к кортежу
+# подсчитать частоту встречающихся букв в строке
+# вывести на экран в формате "символ: количество вхождений"
+
+string_1 = input('Введите текст: ')
+list_1 = []
+print(string_1)
+for i in string_1:
+    if i not in list_1 and i.isalpha():
+        print('Символ: "' + i + '" количество вхождений: ' + str(string_1.count(i)))
+        list_1.append(i)
+print(tuple(list_1))
