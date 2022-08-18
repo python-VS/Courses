8388d7edd4e6ec3d25f1444c8a0fae33dbf48db0
vitali_saharov <vitali.saharov@gmail.com>
2022-08-18 08:38:16 +0300

    Merge branch 'dict'
    # Conflicts:
    #	homework/Tuples/TuplesTask2.py

--- TasksControl/Task2Aug/Task1.py
+++ TasksControl/Task2Aug/Task1.py
@@ -1,7 +1,15 @@
-my_tuple = (1, 3, 5, 2)
+# вывести на экран
+# если хотя бы один элемент не целое число - вывести исх.кортеж
 
+
+from random import randint
+
+my_tuple = tuple([randint(1, 100) for _ in range(10)])
+#my_tuple += ('a', 1.1,)
+print(my_tuple)
 for i in my_tuple:
-    if i == float():
-        print(my_tuple)
-    else:
-        print(sorted(tuple(my_tuple)))
+    if type(i) != int:
+        print("Обнаружено нецелое число", my_tuple)
+        break
+else:
+    print('Отсортированный кортеж: ', sorted(my_tuple))
