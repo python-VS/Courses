0be2894825c2ade59c2370a2fe078abf205ec216
vitali_saharov <vitali.saharov@gmail.com>
2022-08-29 01:09:46 +0300

    TuplesTask1 renamed & changed

--- homework/Tuples/tuples_task_2.py
+++ homework/Tuples/tuples_task_2.py
@@ -5,9 +5,11 @@
 
 string_1 = input('Введите текст: ')
 list_1 = []
-print(string_1)
+print('Введенный текст: ', string_1)
+
 for i in string_1:
     if i not in list_1 and i.isalpha():
         print('Символ: "' + i + '" количество вхождений: ' + str(string_1.count(i)))
         list_1.append(i)
+
 print(tuple(list_1))
