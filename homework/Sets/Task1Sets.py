6c2375c373114d6d21b62010991d16dd9a7004f6
vitali_saharov <vitali.saharov@gmail.com>
2022-09-03 17:40:53 +0300

    Sets Tasks complited

--- homework/Sets/Task1Sets.py
+++
@@ -1,5 +0,0 @@
-from collections import Hashable
-
-list_1 = [1, 'a', 5, 3.2]
-x = {i for i in list_1 if isinstance(i, Hashable)}
-print(x)
--- homework/Sets/Task3Sets.py
+++
@@ -1,5 +0,0 @@
-from random import randint
-
-list_1 = [randint(0, 10000) for _ in range(0, 10000)]
-list_2 = [randint(0, 10000) for _ in range(0, 10000)]
-print(sorted(list(set(list_1) & set(list_2))))
