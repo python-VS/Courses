b0d49c94ea1cf2958b42dcfb45a535a1affde8c0
vitali_saharov <vitali.saharov@gmail.com>
2022-09-05 02:05:04 +0300

    Dicts Tasks changed

--- homework/Dictionaries/DictTask1.py
+++
@@ -1,4 +0,0 @@
-dict_1 = {'a':300, 'b':400}
-dict_2 = {'c':300, 'd':600}
-print(dict_1.update(dict_2))
-print(dict_1)
--- homework/Dictionaries/DictTask4.py
+++
@@ -1,2 +0,0 @@
-lis_1, list_2 = ['a', 'b', 'y', 'r', 't'], [8, 4, 7, 10, 3]
-print(dict(zip(lis_1, list_2)))
