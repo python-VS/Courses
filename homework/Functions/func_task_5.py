600ad13a80e40f5c7e9450baffd7c9b1dace4d99
vitali_saharov <vitali.saharov@gmail.com>
2022-09-22 19:28:03 +0300

    nothing

--- homework/Functions/func_task_5.py
+++ homework/Functions/func_task_5.py
@@ -1,17 +1,16 @@
 # Написать функцию-декоратор, которая вычисляет время выполнения декорируемой функции,
 # а также выводит на экран имя функции и ее параметры.
 
-import time
+def my_dec(func):
+    def wrapper(*args, **kwargs):
+        # operations_before_run
+        result = func(*args, **kwargs)
+        # operations_after_run
+        return result
+    return wrapper
 
-def decorator(func):
-    def inner():
-        print('tttttttt')
-        func()
-        print('gggggggggg')
-    return inner()
+#Декорирование функции происходит посредством следующего синтаксиса
 
-def say():
-    print('15651')
-
-say = decorator(say)
-say()
+@decorator
+def func():
+    pass
