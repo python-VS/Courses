6f7ee30c9c1d20334b80ce170924bb2dc45d9b48
Vitali <vitali.saharov@gmail.com>
2022-10-20 01:37:10 +0300

    functions task 2 completed

--- homework/Functions/func_task_2.py
+++ homework/Functions/func_task_2.py
@@ -4,5 +4,20 @@
 # -название анализируемой функции,
 # -наименование всех принимаемых ею параметров и их типы (позиционные, ключевые и т.п.)
 
-def inspect_function():
-    pass
+import inspect
+from collections.abc import Callable
+
+
+def sum_range(_start: int, _end: int, _step: int = 0) -> int:
+    _start, _end = (_start, _end) if _start <= _end else (_end, _start)
+    return sum(range(_start, _end, _step))
+
+
+def inspect_function(func: Callable):
+    print(f'Название функции: {func.__name__}')
+    print('Принимаемые параметры:')
+    for param in inspect.signature(func).parameters.values():
+        print(' - ' + param.name, param.kind, sep=': ')
+
+
+inspect_function(sum_range)
