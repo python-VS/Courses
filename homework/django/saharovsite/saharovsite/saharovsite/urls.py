a9d86bb50dfaf39bdada56dd656e7ea575ad9ff6
Vitali <vitali.saharov@gmail.com>
2022-10-31 01:39:23 +0300

    django tasks project&app created2

--- homework/django/saharovsite/saharovsite/saharovsite/urls.py
+++ homework/django/saharovsite/saharovsite/saharovsite/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from saharapp.views import index
+from django.urls import path, include
 
 urlpatterns = [
-    path('saharapp/', index),
+    path('saharapp/', include(saharapp.urls)),
     path('admin/', admin.site.urls),
 ]
