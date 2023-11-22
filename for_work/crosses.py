5beb7a0564e3a9a9b2dc666ba8c14ea5022d15db
vitali <vitali.saharov@gmail.com>
2023-11-22 16:25:45 +0300

    +Tk++cycle cutting \r in list everything is working nice

--- for_work/crosses.py
+++ for_work/crosses.py
@@ -1,8 +1,6 @@
-from tkinter import Tk, ttk, Button, Label, END, Text, BOTH, Frame
+from tkinter import Tk, ttk, END, Text
 import pyperclip, re
 
-
-data = ('551125B_CORTECO', '640VE31280000_MAHLE', '701010_TRW ENGINE COMPONENT', 'PV1456_PATRON', 'R6275/SNT_FRECCIA', 'V004307_BGA')
 
 def insert_text_buff():
     past = pyperclip.paste()
@@ -20,43 +18,44 @@ def delete_text_out():
 
 def get_text_input():
     in_text_content = in_text.get(0.0, END)
-    list1 = set(in_text_content.split(sep='\n'))
+    list1 = list(in_text_content.split(sep='\n'))
     list2 = []
 
     for w in list1:
         w = re.sub(r'\r', r'', w)
-        list2.append(w)
+        if w != '':
+            list2.append(w)
+        else:
+            pass
 
     for i in list2:
         for x in list2:
-            if i != x and i != '':
+            if i != x:
                 z = f'{i}\t{x}\n'
                 out_text.insert(0.0, z)
             else:
                 pass
-    print(in_text_content, type(in_text_content))
-    print(list1, type(list1))
-    print(list2, type(list2))
 
-root = Tk()
-root.title('Crosses')
-root.geometry("700x850+400+130")  # ширина х высота + позиция х\у
-root.resizable(False, False)  # растягивание границ окна
-#root.iconbitmap(default="favicon.ico")
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Crosses')
+    root.geometry("700x850+400+130")  # ширина х высота + позиция х\у
+    root.resizable(False, False)  # растягивание границ окна
+    #root.iconbitmap(default="favicon.ico")
 
-in_text = Text(root, wrap='word', width=80)
-in_text.grid(row=0, column=0, columnspan=5)
+    in_text = Text(root, wrap='word', width=80)
+    in_text.grid(row=0, column=0, columnspan=5)
 
 
-btn1 = ttk.Button(root, text='Вставить', command=insert_text_buff).grid(row=1, column=1)
-btn2 = ttk.Button(root, text='Очистить', command=delete_text_in).grid(row=1, column=2)
-btn3 = ttk.Button(root, text='Обработать', command=get_text_input).grid(row=1, column=3)
+    btn1 = ttk.Button(root, text='Вставить', command=insert_text_buff).grid(row=1, column=1)
+    btn2 = ttk.Button(root, text='Очистить', command=delete_text_in).grid(row=1, column=2)
+    btn3 = ttk.Button(root, text='Обработать', command=get_text_input).grid(row=1, column=3)
 
-out_text = Text(root, wrap='word')
-out_text.grid(row=2, column=0, columnspan=5)
+    out_text = Text(root, wrap='word')
+    out_text.grid(row=2, column=0, columnspan=5)
 
-btn4 = ttk.Button(root, text='Скопировть', command=copy_text_buff).grid(row=3, column=1)
-btn5 = ttk.Button(root, text='Очистить', command=delete_text_out).grid(row=3, column=2)
-btn6 = ttk.Button(root, text='Сохранить').grid(row=3, column=3)
+    btn4 = ttk.Button(root, text='Скопировть', command=copy_text_buff).grid(row=3, column=1)
+    btn5 = ttk.Button(root, text='Очистить', command=delete_text_out).grid(row=3, column=2)
+    btn6 = ttk.Button(root, text='Сохранить').grid(row=3, column=3)
 
-root.mainloop()
+    root.mainloop()
